bc9dde25ce580b295f036e991db474c4c43c9858
Cypax <github@cypax.net>
2019-04-10 17:03:13 +0200

    initial check-in of request client

---
+++ code/aquaRequest/ClientRequest.cpp
@@ -0,0 +1,202 @@
+/*************************************************************************************//*!@addtogroup file Files*//*@{*/
+/*!
+ ***********************************************************************************************************************
+* File identification:      $Id: ClientRequest.cpp 37 2017-12-13 14:41:26Z cypax $
+* Revision of last commit:  $Rev: 37 $
+* Author of last commit:    $Author: cypax $
+* Date of last commit:      $Date: 2017-12-13 15:41:26 +0100 (Mi, 13 Dez 2017) $
+ ***********************************************************************************************************************
+ *
+ * @project     Raspberry Pi Aquarium Control
+ *
+ * @author      Cypax / cypax.net
+ * @copyright
+ *
+ * @brief       Sets up a QTcpServer. Passes client requests to a RelayControl object.
+ */
+/****************************************************************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Library includes                                                                                                   */
+/**********************************************************************************************************************/
+#include <QtDebug>
+#include <QDataStream>
+#include <QTimer>
+
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+/**********************************************************************************************************************/
+/* Own includes                                                                                                       */
+/**********************************************************************************************************************/
+#include "ClientRequest.h"
+
+/**********************************************************************************************************************/
+/* Namespaces                                                                                                         */
+/**********************************************************************************************************************/
+
+/**********************************************************************************************************************/
+/* Constants (#define)                                                                                                */
+/*******************************************************************************//*!@addtogroup define Constants*//*@{*/
+#define DEBUG_ClientRequest 1
+#if DEBUG_ClientRequest
+#define QDEBUG qDebug()<<__func__<<"()\t"
+#else
+#define QDEBUG if (0) qDebug()
+#endif
+/***** End of: define Constants *********************************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Constructors                                                                                                       */
+/**************************************************************************//*!@addtogroup constrc Constructors *//*@{*/
+ClientRequest::ClientRequest(long lPort, QString strRequest)
+{
+  m_Request = strRequest;
+  QDEBUG << strRequest;
+
+  m_pSocket = new QTcpSocket();
+
+  connect(m_pSocket, SIGNAL(readyRead()), this, SLOT(handleReceive()));
+  connect(m_pSocket, SIGNAL(disconnected()), this, SLOT(handleDisconnect()));
+  connect(m_pSocket, SIGNAL(connected()), this, SLOT(sendRequest()));
+  connect(m_pSocket, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(handleDisconnect()));
+
+#ifdef Q_OS_WIN
+#warning "WARNING: FIT is active!"
+  m_pSocket->connectToHost("10.0.80.10", lPort);
+#else
+  m_pSocket->connectToHost("localhost", lPort);
+#endif
+
+
+  // terminate on timeout
+  QTimer::singleShot(2000, Qt::VeryCoarseTimer, this, SLOT(handleTimeout()));
+}
+
+/***** End of: constrc Constructors *****************************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Deconstructor                                                                                                      */
+/**********************************************************************//*!@addtogroup deconstrc Deconstructors *//*@{*/
+ClientRequest::~ClientRequest()
+{
+  if (Q_NULLPTR != m_pSocket)
+  {
+    QDEBUG << "disconnecting from backend";
+    m_pSocket->close();
+    delete m_pSocket;
+  }
+}
+/***** End of: deconstrc Constructors ***************************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Public functions                                                                                                   */
+/*********************************************************************//*!@addtogroup pubfunct Public functions *//*@{*/
+/***** End of: pubfunct Public functions ************************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Protected functions                                                                                                */
+/*****************************************************************//*!@addtogroup protfunct Protected functions *//*@{*/
+/***** End of: protfunct Protected functions ********************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Private functions                                                                                                  */
+/*****************************************************************//*!@addtogroup privfunct Private functions   *//*@{*/
+/***** End of: privfunct Private functions **********************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Public slots                                                                                                       */
+/*****************************************************************//*!@addtogroup pubslots Public slots         *//*@{*/
+/***** End of: pubslots Public slots ****************************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Protected slots                                                                                                    */
+/*************************************************************//*!@addtogroup protslots Protected slots         *//*@{*/
+
+void ClientRequest::sendRequest(void)
+{
+  if (Q_NULLPTR != m_pSocket)
+  {
+    if (m_pSocket->isOpen())
+    {
+      QDEBUG << "sending request";
+
+      QByteArray block;
+      QDataStream out(&block, QIODevice::WriteOnly);
+      out.setVersion(QDataStream::Qt_4_0);
+      out << (quint16)0;
+
+      out << m_Request.trimmed();
+
+
+      out.device()->seek(0);
+      out << (quint16)(block.size() - sizeof(quint16));
+
+      m_pSocket->write(block);
+
+      QDEBUG << block;
+    }
+  }
+}
+
+void ClientRequest::handleTimeout(void)
+{
+  QDEBUG << "timeout";
+  emit signalTerminate();
+}
+
+void ClientRequest::handleDisconnect(void)
+{
+  QDEBUG << "disconnect";
+  emit signalTerminate();
+}
+
+void ClientRequest::handleReceive(void)
+{
+  quint16 blockSize = 0;
+  QDataStream in(m_pSocket);
+  in.setVersion(QDataStream::Qt_4_0);
+
+  // read bock size
+  if (blockSize == 0)
+  {
+      if (m_pSocket->bytesAvailable() < (int)sizeof(quint16))
+      {
+        QDEBUG << "Message too short";
+        return;
+      }
+      in >> blockSize;
+  }
+
+  if (m_pSocket->bytesAvailable() < blockSize)
+  {
+    QDEBUG << "Message payload too short";
+    return;
+  }
+
+  QString serverResponse;
+
+  in >> serverResponse;
+  serverResponse = serverResponse.trimmed();
+
+  QDEBUG << serverResponse;
+
+  const char* retStr = serverResponse.toUtf8().constData();
+  cout << retStr;
+
+  emit signalTerminate();
+  //emit signalReceive(serverResponse);
+}
+
+
+/***** End of: protslots Protected slots ************************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Private slots                                                                                                      */
+/*****************************************************************//*!@addtogroup privslots Private slots       *//*@{*/
+/***** End of:privslots Private slots ***************************************************************************//*@}*/
+
+/**** Last line of source code                                                                                     ****/
---
+++ code/aquaRequest/main.cpp
@@ -0,0 +1,187 @@
+/*************************************************************************************//*!@addtogroup file Files*//*@{*/
+/*!
+ ***********************************************************************************************************************
+* File identification:      $Id: main.cpp 45 2017-12-13 15:37:51Z cypax $
+* Revision of last commit:  $Rev: 45 $
+* Author of last commit:    $Author: cypax $
+* Date of last commit:      $Date: 2017-12-13 16:37:51 +0100 (Mi, 13 Dez 2017) $
+ ***********************************************************************************************************************
+ *
+ * @project     Raspberry Pi Aquarium Control
+ *
+ * @author      Cypax / cypax.net
+ * @copyright
+ *
+ * @brief       Contains main() function of the software. Sends a TCP request.
+ */
+/****************************************************************************************************************//*@}*/
+
+/**********************************************************************************************************************/
+/* Library Includes                                                                                                   */
+/**********************************************************************************************************************/
+#include <QCoreApplication>
+#include <QObject>
+#include <QtGlobal>
+#include <QDebug>
+
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+/**********************************************************************************************************************/
+/* Own Includes                                                                                                       */
+/**********************************************************************************************************************/
+#include "ClientRequest.h"
+
+/**********************************************************************************************************************/
+/* Defines                                                                                                            */
+/**********************************************************************************************************************/
+#define DEBUG_MAIN 1
+#if DEBUG_MAIN
+#define QDEBUG qDebug()<<__func__<<"()\t"
+#else
+#define QDEBUG if (0) qDebug()
+#endif
+
+#define MIN_ARGC_VALUE    1
+#define ARG_PORT          "-p"
+#define ARG_USAGE         "-?"
+#define DEFAULT_TCP_PORT  (long)58236
+#define MIN_PORT_VALUE    (long)1024
+#define MAX_PORT_VALUE    (long)65535
+
+enum iProgramExitCodes
+{
+  iProgramExitSuccess =          (int)0,
+  iProgramInvalidArgument =      (int)1,
+  iProgramArgPortWithoutNumber = (int)2,
+  iProgramArgPortWithInvalidNo = (int)3,
+  iProgramArgHostWithoutName   = (int)4,
+  iProgramAddToQtReturnValue   = (int)5
+};
+
+/**********************************************************************************************************************/
+/* Global Variables Definitions                                                                                       */
+/**********************************************************************************************************************/
+
+
+/**********************************************************************************************************************/
+/* Private Variables                                                                                                  */
+/**********************************************************************************************************************/
+
+
+/**********************************************************************************************************************/
+/* Private Functions-Prototypes                                                                                       */
+/**********************************************************************************************************************/
+
+
+
+/**********************************************************************************************************************/
+/* Private Functions                                                                                                  */
+/**********************************************************************************************************************/
+void printUsage(void)
+{
+  QDEBUG << "USAGE:\n"
+         "[-p <port>] [-h <host>] [-v] [-?]\n"
+         "  -p <port>     Defines the listening port of the backend. (default: 58236)\n"
+         "  -?            This help text.\n"
+         ;
+}
+
+/**********************************************************************************************************************/
+/* Global Functions                                                                                                   */
+/**********************************************************************************************************************/
+/**
+ * @brief main
+ * @param argc: number of arguments passed to the programm + 1
+ * @param argv: array of pointers to the arguments
+ * @return int: Integer value passed to the operating system after program termination
+ */
+int main(int argc, char *argv[])
+{
+  int retVal = iProgramExitSuccess;
+
+  // Declaration of main Qt Application
+  QCoreApplication coreApp(argc, argv);
+
+  // Variables to store arguments passed to program
+  long lPort = DEFAULT_TCP_PORT;
+
+  // Read arguments and use them to configure the program
+  if (1 < argc)
+  {
+    for (quint16 i = 1; i < argc; i++)
+    {
+      if (0 == strcmp(ARG_USAGE, argv[i]))
+      {
+        printUsage();
+        return iProgramExitSuccess;
+      }
+      else if (0 == strcmp(ARG_PORT, argv[i]))
+      {
+        // If port configuration argument was passed, there must be a port no. following
+        if (i < (argc -1))
+        {
+          i++;
+          lPort = strtol(argv[i], 0, 10);
+          if (lPort < MIN_PORT_VALUE || lPort > MAX_PORT_VALUE)
+          {
+            printUsage();
+            return iProgramArgPortWithInvalidNo;
+          }
+        }
+        else
+        {
+          printUsage();
+          return iProgramArgPortWithoutNumber;
+        }
+      }
+      else
+      {
+        printUsage();
+        return iProgramInvalidArgument;
+      }
+    }
+  }
+
+
+  cout << "Content-type:text/html\r\n\r\n";
+
+  // attempt to retrieve value of query string
+  char* cpQuery = getenv("QUERY_STRING");
+
+#ifdef Q_OS_WIN
+#warning "WARNING: FIT is active!"
+  cpQuery = (char*)"request=getStatus";
+  //cpQuery = (char*)"request=setRelayOverwrite6|30,OFF";
+  //cpQuery = (char*)"request=getRelayState0";
+  //cpQuery = (char*)"request=setRelayMode2|SCHEDULE";
+  //cpQuery = (char*)"request=getRelaySchedule0";
+  //cpQuery = (char*)"request=setRelaySchedule7|111111111111111111111111,111111111111111111111111,111111111111111111111111,111111111111111111111111,111111111111111111111111,111111111111111111111111,111111111111111111111111";
+#endif
+
+
+  if ( cpQuery != NULL )
+  {
+    ClientRequest clientRequest(lPort, QString(cpQuery).trimmed());
+
+    // exit application when server receives terminate request
+    coreApp.connect(&clientRequest, SIGNAL(signalTerminate()), &coreApp, SLOT(quit()));
+
+    retVal = coreApp.exec();
+  }
+  else
+  {
+    //Environment variable does not exist.
+  }
+
+
+  QDEBUG << "Exit with code" << retVal;
+  return retVal;
+}
+/**********************************************************************************************************************/
+
+
+/************************* End of File ********************************************************************************/
